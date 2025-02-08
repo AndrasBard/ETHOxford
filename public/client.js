e6c2e4dbf5fae1cabd7c0cfabbf06627846ee9a2
Andras Bard <91786910+AndrasBard@users.noreply.github.com>
2025-02-08 12:43:46 +0000

    Add modal for entering key

--- public/client.js
+++ public/client.js
@@ -1,1 +1,10 @@
 console.log("Hello world!");
+
+const dialog = document.querySelector('dialog');
+//dialog.showModal();
+
+const form = document.querySelector('dialog form');
+const key = document.querySelector('#key');
+form.addEventListener('submit', (event) => {
+    document.cookie = `key=${key.value}; max-age=3600`;
+});
